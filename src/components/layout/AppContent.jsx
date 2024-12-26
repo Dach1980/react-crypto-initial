fb4e17275807d37239bd7f66e560b9bee14aef2d
Dmitry Cherkashin <dchpost@mail.ru>
2024-12-26 10:26:13 +1000

    добавляет общую сумму в Content

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,4 +1,6 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+import { useCrypto } from '../context/crypto-context';
+
 const contentStyle = {
     textAlign: 'center',
     minHeight: 'calc(100vh - 60px)',
@@ -8,8 +10,22 @@ const contentStyle = {
 };
 
 export default function AppContent() {
-    return (
+    const { assets, crypto } = useCrypto()
 
-        <Layout.Content style={contentStyle}>Content</Layout.Content>
+    return (
+        <Layout.Content style={contentStyle}>
+            <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
+            Portfolio:{' '}
+                {assets
+                    .map((asset) => {
+                        const coin = crypto.find((c) => c.id === asset.id)
+                        return asset.amount * coin.price
+                    })
+                    .reduce((acc, v) => (acc += v), 0)
+                    .toFixed(2)
+                }
+                $
+            </Typography.Title>
+        </Layout.Content>
     )
 }
